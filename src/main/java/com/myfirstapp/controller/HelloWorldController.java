7b7ae97bd7bbb6d041ca38ef5587d9538a2d07fe
pachgadehardik <pachgadehardik@gmail.com>
2020-11-23 23:21:59 +0530

    Added Springboot Based Demo Project

---
+++ src/main/java/com/myfirstapp/controller/HelloWorldController.java
@@ -0,0 +1,35 @@
+package com.myfirstapp.controller;
+
+import com.myfirstapp.model.User;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController {
+
+    @RequestMapping(value = {"/home"})
+    public String sayHello(){
+        return "Hello -World";
+    }
+
+    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value="name") String name,@RequestParam(value="lname") String lname){
+        return "Hello "+ name+" "+lname+"!!";
+    }
+
+    @GetMapping("/param/{name}")
+    public String sayHelloParam(@PathVariable String name){
+        return "Hello "+ name+"!!";
+    }
+
+    @PostMapping("/post")
+    public String sayHello(@RequestBody User user){
+        return "Hello "+ user.getFirstName()+ " "+user.getLastName()+"!!";
+    }
+
+    @PutMapping("/put/{firstName}")
+    public String sayHelloPut(@PathVariable String firstName, @RequestParam(value="lastName") String lastName){
+        return "Hello "+ firstName+ " "+lastName+"!!";
+    }
+
+}
